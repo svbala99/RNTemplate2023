747029d285c20b8512e588b8d785214195897b34
Balamurugan <svbala99@gmail.com>
2022-12-15 15:43:25 +0530

    Linting configs done

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,14 @@
 module.exports = {
-  arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
   trailingComma: 'all',
+  arrowParens: 'always',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  jsxSingleQuote: true,
+  quoteProps: 'as-needed',
+  singleQuote: true,
+  semi: true,
+  // printWidth: 80,
+  useTabs: true,
+  tabWidth: 2,
 };
